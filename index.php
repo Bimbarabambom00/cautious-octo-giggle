3c0fbc5f06be9a207a1f68108379e4a266c93262
Bimbarabambom00 <97984580+Bimbarabambom00@users.noreply.github.com>
2022-01-29 17:32:29 +0100

    Add files via upload

---
+++ index.php
@@ -0,0 +1,28 @@
+<!doctype html>
+<html lang="pl">
+	<head>
+		<script src="plik.js"></script> 
+		<meta charset="utf-8">
+		<title>Input Form UI Design</title>
+		<link rel="stylesheet" href="style.css">
+	</head>
+	<body>
+	<?php 
+		echo "Hello World! ";
+	?>
+		<div class="box">
+			<h2>Login</h2>
+			<form>
+				<div class="inputBox">
+					<input type="text" name="" required="">
+					<label>Username</label>
+				</div>
+				<div class="inputBox">
+					<input type="Password" name="" required="">
+					<label>Password</label>
+				</div>
+				<input type="submit" name="" value="Submit">
+			</form>
+		</div>
+	</body>
+</html>
