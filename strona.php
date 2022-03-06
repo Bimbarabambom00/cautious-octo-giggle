6a7f7b8f90610235b1eeb3c3864a9cefd5bfc771
Bimbarabambom00 <97984580+Bimbarabambom00@users.noreply.github.com>
2022-03-06 01:13:27 +0100

    Add files via upload

--- strona.php
+++ strona.php
@@ -1,4 +1,10 @@
 <html>
+<head>
+    <link rel="stylesheet" href="filecss.css"> 
+    <script src="filejs.js"></script> 
+    <meta charset="UTF-8">
+    <title>Wataha Studio</title>
+</head>
 <body>
 Hello
 </body>
