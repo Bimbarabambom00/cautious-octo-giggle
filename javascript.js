f20401ccb4945c83267ff94b856109cbc2d82834
Bimbarabambom00 <97984580+Bimbarabambom00@users.noreply.github.com>
2022-03-25 22:00:53 +0100

    Add files via upload

---
+++ javascript.js
@@ -0,0 +1,17 @@
+		const zaloguj = () => {
+				<!-- ADMIN -->
+			if (document.getElementById("login_usr").value == "fortinajta" && document.getElementById("password_usr").value == "lababadzi"){
+				alert("witam");
+				
+				}
+				<!-- ADAM -->
+			if (document.getElementById("login_usr").value == "mada" && document.getElementById("password_usr").value == "passadmin"){
+				alert("pomyślnie zalogowano jako ADAM.admin");
+			
+				}
+				<!-- KONRAD -->
+			if (document.getElementById("login_usr").value == "komrad" && document.getElementById("password_usr").value == "adminpass"){
+				alert("pomyślnie zalogowano jako KACPER.admin");
+			
+				}
+		}
